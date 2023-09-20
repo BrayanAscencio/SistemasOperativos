c343bad162e499df6cb4ea277d8066ee4115d300
Brayan Ascencio <133992033+BrayanAscencio@users.noreply.github.com>
2023-09-19 23:58:02 -0500

    comandos.sh

--- menu/menu/comandos.sh
+++ menu/menu/comandos.sh
@@ -9,6 +9,22 @@ show_menu() {
     echo "2. Mostrar HolaMundo"
     echo "3. Mostrar Nombre"
     echo "4. Exit"
+    echo "5. Comprobaciones"
+    echo "6. Ejemplo de Variables"
+    echo "7. Lógica"
+    echo "8. Aritmética"
+    echo "9. Usos de Variables"
+    echo "10. Arrays"
+    echo "11. Ejemplo de Algunas Cosas con Variables"
+    echo "12. Captura de Señales"
+    echo "13. Condicionales y Test"
+    echo "14. Comprobaciones Complejas"
+    echo "15. Estructura CASE"
+    echo "16. Iteración FOR"
+    echo "17. Iteración WHILE"
+    echo "18. Iteración UNTIL"
+    echo "19. Iteración SELECT"
+    echo "20. Ejemplos de Funciones"
     echo
 }
 
@@ -34,9 +50,10 @@ while true; do
             echo "Exit"
             exit 0
             ;;
-        *)
-            echo "Yamete Kudasay "
+        5)
+            echo "Aritmetica "
             read -p "Presione Enter para seguir..."
             ;;
+            
     esac
 done
